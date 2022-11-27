273b81ec1e43a6a8da52aaddb397ae702e081e93
GossipSub FM <108960361+gossipsubfm@users.noreply.github.com>
2022-11-27 16:56:26 -0700

    Update msgs-state.lisp

--- msgs-state.lisp
+++ msgs-state.lisp
@@ -12,16 +12,15 @@
 (defdata msgpeer-age (alistof msg-peer rational))
 
 (check= (msgpeer-agep
-	 '( (((|100| |10| FM AN) MX) . 2)
+         '( (((|100| |10| FM AN) MX) . 2)
             ((|10| P2) . 10)
             (((|3lolol| |10| PL MX) P2) . 90)
             ((|12| P3) . 10)))
         t)
 
-
 (defdata mcache (listof payload-peer))
 (check= (mcachep '(((|1000| |10| PL MX)  P3)
-		   ((|2000| |10| NT PT) P4)))
+                   ((|2000| |10| NT PT) P4)))
         t)
 
 ;;waiting for payload corresponding to pid sent by peer
@@ -30,59 +29,34 @@
 (defdata msgs-state
   (record (recently-seen . msgpeer-age)
           ;; LRU cache of seen message IDs.
-	  (pld-cache     . mcache)
+          (pld-cache . mcache)
           ;; list of messages
-          (hwindows      . lon)  ;;history windows : each nat is the number of
+          (hwindows . lon) ;;history windows : each nat is the number of
           ;;messages received in the last HeartBeat interval
-	  (waitingfor    . msgs-waiting-for) ;;received IHAVE, sent IWANT
-                                             ;;waiting for full message
-          (served        . msgpeer-age) ;; peers whose IWANT was served. Count
-                                        ;; of servings to avoid spam.
+          (waitingfor . msgs-waiting-for) ;;received IHAVE, sent IWANT
+          ;;waiting for full message
+          (served  . msgpeer-age) ;; peers whose IWANT was served. Count
+          ;; of servings to avoid spam.
           (ihaves-received . nat)
-          (ihaves-sent     . nat)))
+          (ihaves-sent . nat)))
 
+(property msgs-state-check-prop (ms :msgs-state)
+  (^ (lonp (g :hwindows ms))
+     (msgpeer-agep (g :recently-seen ms))
+     (mcachep (g :pld-cache ms))
+     (msgs-waiting-forp (g :waitingfor ms))
+     (msgpeer-agep (g :served ms))
+     (natp (g :ihaves-received ms))
+     (natp (g :ihaves-sent ms)))
+  :rule-classes :forward-chaining)
 
-(defthm lonp->tlp
-  (=> (lonp x)
-      (tlp x)))
+(in-theory (disable msgs-statep))
 
-(defthm nu
-  (=>
-   (AND (NOT (CONSP VAR-HWINDOWS))
-        VAR-HWINDOWS)
-   (not (lonp VAR-HWINDOWS))))
+(definecd increase-ihaves-received (mst :msgs-state n :nat) :msgs-state
+  (s :ihaves-received (+ n (msgs-state-ihaves-received mst)) mst))
 
-
-(defthm msgs-state-check
-  (== (msgs-statep
-       (MSET
-        :0TAG 'MSGS-STATE
-        (MSET
-         :SERVED VAR-SERVED
-         (MSET :HWINDOWS
-               VAR-HWINDOWS
-               (MSET :PLD-CACHE
-                     VAR-PLD-CACHE
-                     (MSET :WAITINGFOR VAR-WAITINGFOR
-                           (MSET :RECENTLY-SEEN
-                                 VAR-RECENTLY-SEEN
-                                 (MSET :IHAVES-RECEIVED VAR-IHAVES-RECEIVED
-                                       (MSET :IHAVES-SENT VAR-IHAVES-SENT
-                                             NIL)))))))))
-      (^ (lonp var-hwindows)
-         (msgpeer-agep var-recently-seen)
-         (mcachep var-pld-cache)
-         (msgs-waiting-forp var-waitingfor)
-         (msgpeer-agep var-served)
-         (natp var-ihaves-received)
-         (natp var-ihaves-sent)))
-  :hints (("Goal" :in-theory (enable msgs-statep))))
-
-(definec increase-ihaves-received (mst :msgs-state n :nat) :msgs-state
-  (mset :ihaves-received (+ n (msgs-state-ihaves-received mst)) mst))
-
-(definec reset-ihaves-received (mst :msgs-state) :msgs-state
-  (mset :ihaves-received 0 mst))
+(definecd reset-ihaves-received (mst :msgs-state) :msgs-state
+  (s :ihaves-received 0 mst))
 
 (defconst *max-want-serves* 3) ;; max number of times IWANTS are served
 (defconst *max-ihaves* 20) ;; max number of ihaves set/received per heartbeat
@@ -103,12 +77,14 @@
          (msgs-waiting-forp l2))
       (msgs-waiting-forp (union-equal l1 l2))))
 
+(sig union-equal ((listof :a) (listof :a)) => (listof :a))
+
 (definecd update-waitingfor (mst :msgs-state pids :lopid sender :peer) :msgs-state
-  (mset :waitingfor (union-equal (mk-pid-peers pids sender)
+  (s :waitingfor (union-equal (mk-pid-peers pids sender)
                                  (msgs-state-waitingfor mst))
         mst))
 
-(definec new-msgs-state () :msgs-state
+(definecd new-msgs-state () :msgs-state
   (msgs-state nil nil nil nil nil 0 0))
 
 (create-map* (lambda (pldm) (if (payload-peerp pldm)
@@ -120,7 +96,6 @@
 
 (property lomsgs (rs :msgpeer-age)
   (lomsg-peerp (strip-cars rs)))
-             
 
 (definecd add-to-recentlyseen (pldm :payload-peer rs :msgpeer-age) :msgpeer-age
   (if (in (second (car pldm))
@@ -128,7 +103,7 @@
       rs
     `((,pldm . 0) . ,rs)))
 
-(definecd update-m-age (rs :msgpeer-age elapsed :pos-rat seenttl :nat) :msgpeer-age
+(definecd update-m-age (rs :msgpeer-age elapsed :pos-rational seenttl :non-neg-rational) :msgpeer-age
   (case-match rs
     (() nil)
     (t (if (> (+ elapsed (cdar rs)) seenttl)
@@ -138,8 +113,8 @@
   
 (definecd in-mcache (pid :pid-type mcache :mcache) :boolean
   (cond ((endp mcache) nil)
-	((== pid (second (caar mcache))) t)
-	(t (in-mcache pid (cdr mcache)))))
+        ((== pid (second (caar mcache))) t)
+        (t (in-mcache pid (cdr mcache)))))
 
 (defconst *waittime* 1)
 
@@ -149,24 +124,24 @@
 (definecd isValidPayload (pld :payload-type) :boolean
   t)
 
-
-
-(definec custom-nth-payload (i :nat top :topic originator :peer) :payload-type
+(definecd custom-nth-payload (i :nat top :topic originator :peer) :payload-type
   (list (intern (concatenate 'string
-                                          "FULL"
-                                          (str::nat-to-dec-string i))
-                             "ACL2")
-                     (intern (concatenate 'string
-                                          "PID"
-                                          (str::nat-to-dec-string i))
-                             "ACL2")
-                     top
-                     originator))
+                             "FULL"
+                             (str::nat-to-dec-string i))
+                "ACL2")
+        (intern (concatenate 'string
+                             "PID"
+                             (str::nat-to-dec-string i))
+                "ACL2")
+        top
+        originator))
 
 (create-map* (lambda (p) (second (car p)))
              mcachep
              lopidp
              (:name get-pids))
+
+(in-theory (disable map*-*get-pids))
 
 (definecd get-plds (pids :lopid mc :mcache) :lopld
   (match mc
@@ -175,6 +150,10 @@
                    (cons (car p)
                          (get-plds pids rst))
                  (get-plds pids rst)))))
+
+(property snd-pld-evnt (p1 p2 :peer pld :payload-type)
+  (evntp (list p1 'snd p2 'payload pld))
+  :hints (("goal" :in-theory (enable evntp))))
 
 (create-map* (lambda (pld p1 p2) `(,p1 SND ,p2 PAYLOAD ,pld))
              lopldp
@@ -211,10 +190,9 @@
                        (pids-to-serve nbr rst servd)
                      (cons pid (pids-to-serve nbr rst servd)))))))
 
-
-(property remove-served-assoc (nbr :peer pid :pid-type servd :msgpeer-age)
-          (msgpeer-agep
-           (remove-assoc-equal (list pid nbr) servd)))
+(sig remove-assoc-equal (:a (alistof :a :b)) => (alistof :a :b))
+(sig remove-duplicates-equal ((listof :a)) => (listof :a))
+(sig intersection-equal ((listof :a) (listof :a)) => (listof :a))
 
 (definec update-served (nbr :peer pids :lopid servd :msgpeer-age) :msgpeer-age
   (match pids
@@ -224,23 +202,43 @@
                                   rst
                                   (if (natp n)
                                       `(((,pid ,nbr) . ,(1+ n))
-  . ,(remove-assoc-equal (list pid nbr) servd))
+                                        . ,(remove-assoc-equal (list pid nbr) servd))
                                     `(((,pid ,nbr) . 1) . ,servd)))))))
-                     
-(property remove-assoc-nbr-counters-map (nbr-counters-map :pt-tctrs-map nbr :peer top :topic)
-          (pt-tctrs-mapp (remove-assoc-equal `(,nbr . ,top) nbr-counters-map)))
 
 
 (create-tuple* msgs-state loev pt-tctrs-map p-gctrs-map)
 
-(skip-proofs
- (definecd update-msgs-state (mst :msgs-state evnt :evnt
+(defthm msgs-state-check2
+  (== (msgs-statep
+       (ss nil
+           :ihaves-received var-ihaves-received
+           :ihaves-sent var-ihaves-sent
+           :pld-cache var-pld-cache
+           :recently-seen var-recently-seen
+           :served var-served
+           :waitingfor var-waitingfor
+           :hwindows var-hwindows
+           :0tag 'msgs-state))
+      (^ (lonp var-hwindows)
+         (msgpeer-agep var-recently-seen)
+         (mcachep var-pld-cache)
+         (msgs-waiting-forp var-waitingfor)
+         (msgpeer-agep var-served)
+         (natp var-ihaves-received)
+         (natp var-ihaves-sent)))
+  :hints (("goal" :in-theory (enable msgs-statep))))
+
+(definecd update-msgs-state1 (mst :msgs-state evnt :evnt
                                   pcmap :pt-tctrs-map
                                   gcmap :p-gctrs-map
                                   twpm :twp)
-   :msgs-stateloevpt-tctrs-mapp-gctrs-map
-   :skip-tests t
-   :timeout 600
+  :msgs-stateloevpt-tctrs-mapp-gctrs-map
+  :skip-tests t
+  :timeout 600
+  :function-contract-hints (("Goal" :DO-NOT-INDUCT T
+                             :in-theory (enable evntp)))
+  :body-contracts-hints (("Goal" :DO-NOT-INDUCT T
+                          :in-theory (enable evntp)))
   (b* ((rs      (msgs-state-recently-seen mst))
        (mcache  (msgs-state-pld-cache mst))
        (hwins   (msgs-state-hwindows mst))
@@ -254,7 +252,7 @@
        (b* ((pldm (list pld p1)))
          (list (msgs-state (add-to-recentlyseen pldm rs)
                            (cons pldm mcache)
-			   (increment-car hwins)
+                           (increment-car hwins)
                            waiting
                            servd
                            ihr
@@ -266,29 +264,29 @@
        (b* ((top (third pld))
             (cs (lookup-tctrs nbr top pcmap))
             ;; don't update firstMessageDeliveries everytime, check mcache
-	    (pldm (list pld nbr)))
-	 (if (isValidPayload pld)
-	     `(,(msgs-state (add-to-recentlyseen pldm rs)
+            (pldm (list pld nbr)))
+         (if (isValidPayload pld)
+             `(,(msgs-state (add-to-recentlyseen pldm rs)
                             (add-ppm-mcache pldm mcache)
-			    (if (in-mcache (payload2pid pld) mcache)
-				hwins
-			      (increment-car hwins))
+                            (if (in-mcache (payload2pid pld) mcache)
+                                hwins
+                              (increment-car hwins))
                             waiting
                             servd
                             ihr
                             ihs)
-	       nil
-	       ,(put-assoc-equal `(,nbr . ,top)
-                                (increment-firstMessageDeliveries
-                                 (increment-meshMessageDeliveries cs))
-                                pcmap)
-               gcmap)
-	   `(,mst
-	     nil
-	     ,(put-assoc-equal `(,nbr . ,top)
+               nil
+               ,(put-assoc-equal `(,nbr . ,top)
+                                 (increment-firstMessageDeliveries
+                                  (increment-meshMessageDeliveries cs))
+                                 pcmap)
+               ,gcmap)
+           `(,mst
+             nil
+             ,(put-assoc-equal `(,nbr . ,top)
                                (increment-invalidMessageDeliveries cs)
                                pcmap)
-             gcmap))))
+             ,gcmap))))
       ((p1 'RCV nbr 'IHAVE pids)
        (b* (((unless (< ihr *max-ihaves*)) default-res)
             (dont-haves
@@ -315,38 +313,140 @@
                            (update-served nbr tsrv servd)
                            ihr
                            ihs)
-                  (map* send-plds (remove-duplicates-equal (get-plds tsrv mcache)) p1 nbr)
-                  pcmap
-                  gcmap)))
+               (map* send-plds (remove-duplicates-equal (get-plds tsrv mcache)) p1 nbr)
+               pcmap
+               gcmap)))
+      (& default-res))))
+
+(sig grab (nat (listof :a)) => (listof :a))
+
+(defthm msgs-state-check3
+  (== (msgs-statep
+       (ss '((:ihaves-received . 0)
+             (:ihaves-sent . 0))
+           :waitingfor var-waitingfor
+           :served var-served
+           :recently-seen var-recently-seen
+           :pld-cache var-pld-cache
+           :hwindows var-hwindows
+           :0tag 'msgs-state))
+      (^ (lonp var-hwindows)
+         (msgpeer-agep var-recently-seen)
+         (mcachep var-pld-cache)
+         (msgs-waiting-forp var-waitingfor)
+         (msgpeer-agep var-served)))
+  :hints (("goal" :in-theory (enable msgs-statep))))
+
+(defthm mcachelen-pos
+  (=> (^ (twpp twpm) twpm)
+      (posp (g :mcachelen (cddr (car twpm)))))
+  :hints (("goal" :in-theory (enable twpp paramsp))))
+
+(defthm helper22
+  (=> (posp x)
+      (^ (integerp x)
+         (<= 0 x))))
+
+(definecd update-msgs-state2 (mst :msgs-state evnt :evnt
+                                  pcmap :pt-tctrs-map
+                                  gcmap :p-gctrs-map
+                                  twpm :twp)
+  :msgs-stateloevpt-tctrs-mapp-gctrs-map
+  :skip-tests t
+  :timeout 600
+  :function-contract-hints (("Goal" :DO-NOT-INDUCT T
+                             :in-theory (enable evntp)))
+  :body-contracts-hints (("Goal" :DO-NOT-INDUCT T
+                          :in-theory (enable evntp twpp)))
+  (b* ((rs      (msgs-state-recently-seen mst))
+       (mcache  (msgs-state-pld-cache mst))
+       (hwins   (msgs-state-hwindows mst))
+       (waiting (msgs-state-waitingfor mst))
+       (servd   (msgs-state-served mst))
+       (ihr     (msgs-state-ihaves-received mst))
+       (ihs     (msgs-state-ihaves-sent mst))
+       (default-res `(,mst nil ,pcmap ,gcmap)))
+    (match evnt
       ((p1 'HBM elapsed)
        (b*  ((wp (cdar twpm))
              (params (cdr wp))
              ((when (null params)) default-res)
              (mcl     (params-mcacheLen params))
-	     (new-hwins (grab mcl (cons 0 hwins)))
+             (new-hwins (grab mcl (cons 0 hwins)))
              (new-mcache (grab (reduce* + 0 new-hwins)
                                mcache))
-	     (seenttl (params-seenTTL params))
-	     (new-mst (msgs-state (update-m-age rs elapsed seenttl)
-				  new-mcache
-				  new-hwins
-				  waiting
+             (seenttl (params-seenTTL params))
+             (new-mst (msgs-state (update-m-age rs elapsed seenttl)
+                                  new-mcache
+                                  new-hwins
+                                  waiting
                                   servd
-                                  0  ;; reset ihaves received 
-                                  0)))  ;;reset ihaves sent.
-       (if (and (consp waiting)
-                (< *waittime* elapsed))
-           (let ((senders (get-ihave-senders waiting)))
-             `(,new-mst
-               nil
-               ;;,(increment-badbehaviours pcmap senders 'FM)))
-               ,pcmap
-               ,gcmap))
-         ;TODO : IHAVE PIDS must also be sent with their topic
-         `(,new-mst
-           nil
-           ,pcmap
-           ,gcmap))))
-      (& default-res)))))
+                                  0    ;; reset ihaves received 
+                                  0))) ;;reset ihaves sent.
+         (if (and (consp waiting)
+                  (< *waittime* elapsed))
+             (let ((senders (get-ihave-senders waiting)))
+               `(,new-mst
+                 nil
+                 ;;,(increment-badbehaviours pcmap senders 'FM)))
+                 ,pcmap
+                 ,gcmap))
+;TODO : IHAVE PIDS must also be sent with their topic
+           `(,new-mst
+             nil
+             ,pcmap
+             ,gcmap))))
+      (& default-res))))
+    
+(definecd update-msgs-state (mst :msgs-state evnt :evnt
+                                 pcmap :pt-tctrs-map
+                                 gcmap :p-gctrs-map
+                                 twpm :twp)
+  :msgs-stateloevpt-tctrs-mapp-gctrs-map
+  :skip-tests t
+  (match evnt
+    ((_ 'HBM _) (update-msgs-state2 mst evnt pcmap gcmap twpm))
+    (& (update-msgs-state1 mst evnt pcmap gcmap twpm))))
 
+(property msgs-stateloevpt-tctrs-mapp-gctrs-map-check
+  (x :msgs-stateloevpt-tctrs-mapp-gctrs-map)
+  (^ (msgs-statep (first x))
+     (loevp (second x))
+     (pt-tctrs-mapp (third x))
+     (p-gctrs-mapp (fourth x))))
+
+
+(property first-update-msgs-state
+  (mst :msgs-state evnt :evnt
+       pcmap :pt-tctrs-map
+       gcmap :p-gctrs-map
+       twpm :twp)
+  (msgs-statep (first (update-msgs-state mst evnt pcmap gcmap twpm))))
+
+(property second-update-msgs-state
+  (mst :msgs-state evnt :evnt
+       pcmap :pt-tctrs-map
+       gcmap :p-gctrs-map
+       twpm :twp)
+  (loevp (second (update-msgs-state mst evnt pcmap gcmap twpm)))
+  :hints (("Goal" :in-theory (enable update-msgs-state))))
+
+(property third-update-msgs-state
+  (mst :msgs-state evnt :evnt
+       pcmap :pt-tctrs-map
+       gcmap :p-gctrs-map
+       twpm :twp)
+  :check-contracts? nil
+  (pt-tctrs-mapp (third (update-msgs-state mst evnt pcmap gcmap twpm)))
+  :hints (("Goal" :in-theory (enable update-msgs-state))))
+
+
+(property fourth-update-msgs-state
+  (mst :msgs-state evnt :evnt
+       pcmap :pt-tctrs-map
+       gcmap :p-gctrs-map
+       twpm :twp)
+  :check-contracts? nil
+  (p-gctrs-mapp (fourth (update-msgs-state mst evnt pcmap gcmap twpm)))
+  :hints (("Goal" :in-theory (enable update-msgs-state))))
 
